f68b36820d3ea27b1c18113df9270891051e3fbe
jallenmanaloto <jallen.manaloto@gmail.com>
2025-02-08 13:30:47 +0800

    feat(main): add application entry point

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use playasia::config::get_config;
+use playasia::server::run;
+
+fn main() -> std::io::Result<()> {
+    let config = get_config().expect("Failed to read config");
+    let address = config.application.connection_string();
+    run(address)
 }
