73a51557bf62e1656745ea0fe0269194b70411d1
jallenmanaloto <jallen.manaloto@gmail.com>
2025-02-08 16:52:20 +0800

    feat(routes): implement protected route
    - Implement protected route using JWT middleware

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
+use crate::middleware::JwtMiddleware;
 use crate::routes::{get_all_items, get_item, health_check};
-use poem::{get, handler, listener::TcpListener, web::Path, Route, Server};
+use poem::{get, handler, listener::TcpListener, web::Path, EndpointExt, Route, Server};
 
 #[handler]
 fn hello(Path(name): Path<String>) -> String {
@@ -12,6 +13,7 @@ pub async fn run(address: String) -> Result<(), std::io::Error> {
         .at("/health", get(health_check))
         .at("/items", get(get_all_items))
         .at("/items/:id", get(get_item))
-        .at("/hello/:name", get(hello));
+        .at("/hello/:name", get(hello))
+        .with(JwtMiddleware::new("secret-key"));
     Server::new(TcpListener::bind(address)).run(app).await
 }
