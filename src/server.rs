dae230d117c84f2cba3f2754925ee1b380abeab5
jallenmanaloto <jallen.manaloto@gmail.com>
2025-02-09 12:53:58 +0800

    feat(app): separate app creation from run

--- src/server.rs
+++ src/server.rs
@@ -1,13 +1,17 @@
 use crate::middleware::JwtMiddleware;
 use crate::routes::{create, delete, edit, get_all_items, get_item, health_check};
-use poem::{get, listener::TcpListener, EndpointExt, Route, Server};
+use poem::{get, listener::TcpListener, Endpoint, EndpointExt, Route, Server};
 
 #[tokio::main]
 pub async fn run(address: String) -> Result<(), std::io::Error> {
-    let app = Route::new()
+    let app = create_app();
+    Server::new(TcpListener::bind(address)).run(app).await
+}
+
+pub fn create_app() -> impl Endpoint {
+    Route::new()
         .at("/health", get(health_check))
         .at("/items", get(get_all_items).post(create))
         .at("/items/:id", get(get_item).put(edit).delete(delete))
-        .with(JwtMiddleware::new("secret-key"));
-    Server::new(TcpListener::bind(address)).run(app).await
+        .with(JwtMiddleware::new("secret-key"))
 }
