96bc6f5e386412a5a87319cb7d7c8946a23cba90
jallenmanaloto <jallen.manaloto@gmail.com>
2025-02-09 12:33:46 +0800

    feats(routes): add route for delete method

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,5 @@
 use crate::middleware::JwtMiddleware;
-use crate::routes::{create, edit, get_all_items, get_item, health_check};
+use crate::routes::{create, delete, edit, get_all_items, get_item, health_check};
 use poem::{get, handler, listener::TcpListener, web::Path, EndpointExt, Route, Server};
 
 #[handler]
@@ -12,7 +12,7 @@ pub async fn run(address: String) -> Result<(), std::io::Error> {
     let app = Route::new()
         .at("/health", get(health_check))
         .at("/items", get(get_all_items).post(create))
-        .at("/items/:id", get(get_item).put(edit))
+        .at("/items/:id", get(get_item).put(edit).delete(delete))
         .at("/hello/:name", get(hello))
         .with(JwtMiddleware::new("secret-key"));
     Server::new(TcpListener::bind(address)).run(app).await
