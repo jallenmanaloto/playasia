c8ad462daae69cd2797ec9418953049ad84afbb7
jallenmanaloto <jallen.manaloto@gmail.com>
2025-02-09 12:50:28 +0800

    refactor(routes): remove boilerplate hello handler

--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,6 @@
 use crate::middleware::JwtMiddleware;
 use crate::routes::{create, delete, edit, get_all_items, get_item, health_check};
-use poem::{get, handler, listener::TcpListener, web::Path, EndpointExt, Route, Server};
-
-#[handler]
-fn hello(Path(name): Path<String>) -> String {
-    format!("hello: {}", name)
-}
+use poem::{get, listener::TcpListener, EndpointExt, Route, Server};
 
 #[tokio::main]
 pub async fn run(address: String) -> Result<(), std::io::Error> {
@@ -13,7 +8,6 @@ pub async fn run(address: String) -> Result<(), std::io::Error> {
         .at("/health", get(health_check))
         .at("/items", get(get_all_items).post(create))
         .at("/items/:id", get(get_item).put(edit).delete(delete))
-        .at("/hello/:name", get(hello))
         .with(JwtMiddleware::new("secret-key"));
     Server::new(TcpListener::bind(address)).run(app).await
 }
